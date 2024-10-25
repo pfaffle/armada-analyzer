d9d42bdf0bcc29934ffd4a00305f830ae13ef292
Craig Meinschein <cmeinsch@gmail.com>
2024-10-24 20:04:39 -0700

    Put in place custom UI

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => setCount(0)}>reset count</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Armada Analyzer</h1>
+      <p>
+        by <a href="https://github.com/pfaffle">pfaffle</a>
       </p>
+      <p className="intro">
+        Export your Star Trek Fleet Command combat logs and upload them here to
+        get an analysis of how your ship and crew performed in battle!
+      </p>
+      <div className="footer">
+        <a href="http://github.com/pfaffle/armada-analyzer">Source</a>
+      </div>
     </>
   );
 }
