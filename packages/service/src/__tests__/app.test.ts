55fd5d43d560640a1e8291bb3491ac36cb405ce1
Craig Meinschein <cmeinsch@gmail.com>
2024-10-27 22:50:13 -0700

    Remove placeholder UI
    The service doesn't need a placeholder UI anymore, remove it

--- packages/service/src/__tests__/app.test.ts
+++ packages/service/src/__tests__/app.test.ts
@@ -6,11 +6,4 @@ describe("basic app", () => {
   it("responds to /healthz", async () => {
     await request(app).get("/healthz").expect(204);
   });
-  it("serves the placeholder static site", async () => {
-    const response = await request(app)
-      .get("/")
-      .expect("content-type", /text\/html/)
-      .expect(200);
-    expect(response.text).toMatch(/Armada Analyzer/);
-  });
 });
--- packages/service/src/app.ts
+++ packages/service/src/app.ts
@@ -1,17 +1,13 @@
 import express from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import url from "url";
 
 function initApp() {
   const app = express();
-  const dirname = path.dirname(url.fileURLToPath(import.meta.url));
   app.use(logger("dev"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-  app.use(express.static(path.join(dirname, "public")));
   app.get("/healthz", (req, res, next) => {
     res.status(204).send();
   });
