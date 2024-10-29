b7fe3c03c1142cbb7a40975fef35e507f0f92e72
Craig Meinschein <cmeinsch@gmail.com>
2024-10-29 00:02:39 -0700

    Add basic file upload UI
    It's unstyled and ugly but works. Will add tests and prettify it in a
    subsequent step.

--- packages/ui/src/FileUploader.tsx
+++ packages/ui/src/FileUploader.tsx
@@ -1,37 +1,62 @@
-import { MouseEventHandler, useState } from "react";
+import { ChangeEventHandler, MouseEventHandler, useState } from "react";
+
+type UploadStatus = "waiting" | "uploading" | "success" | "error";
+
+function statusToMessage(status: UploadStatus) {
+  switch (status) {
+    case "waiting":
+      return "";
+    case "uploading":
+      return "Uploading...";
+    case "success":
+      return "Upload successful!";
+    case "error":
+      return "Upload failed!";
+  }
+}
 
 export const FileUploader = () => {
-  const [fileName, setFileName] = useState<string>();
+  const [status, setStatus] = useState<UploadStatus>("waiting");
+  const [file, setFile] = useState<File>();
+
   const handleSubmit: MouseEventHandler<HTMLInputElement> = (e) => {
     e.preventDefault();
-    if (fileName) {
+    if (file) {
+      setStatus("uploading");
       const formData = new FormData();
-      formData.set("file", fileName);
-      console.log("submitting", fileName);
-      fetch("http://localhost:3000/armada/upload", {
+      formData.set("file", file);
+      fetch(new URL("/armada/upload", import.meta.env.VITE_API_URL), {
         method: "POST",
         body: formData,
       })
         .then(() => {
-          console.log("submit successful!");
+          setStatus("success");
         })
         .catch((err) => {
           console.error(err);
+          setStatus("error");
         });
     }
   };
+  const handleFileSelect: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setStatus("waiting");
+    setFile(e.currentTarget.files?.item(0) || undefined);
+  };
   return (
     <form encType="multipart/form-data">
-      <div>
-        <label htmlFor="file">Select a combat log to upload</label>
+      <div className="segment">
+        <label htmlFor="file">Select a combat log to upload:</label>
       </div>
-      <input
-        type="file"
-        id="file"
-        accept="text/csv"
-        onChange={(e) => setFileName(e.currentTarget.value)}
-      ></input>
-      <input type="submit" onClick={handleSubmit}></input>
+      <div className="segment">
+        <input
+          type="file"
+          id="file"
+          accept="text/csv"
+          onChange={handleFileSelect}
+        />
+        <input type="submit" onClick={handleSubmit} value="Upload" />
+      </div>
+      <div>{statusToMessage(status)}</div>
     </form>
   );
 };
